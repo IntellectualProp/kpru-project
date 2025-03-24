671cde8d5d615050f925790824cce08578bc0859
Naem Supawat <intellectualp08@gmail.com>
2025-03-25 01:05:37 +0700

    [UPDATE] triup section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
             <img src="/images/SAP-Assessment.png" alt="Assessment Image" className="customImage"></img>
           </div>
           <div className="assessment-content">
-            <h3 className="assessment-title">ร่วมประเมินแบบสอบถามความพึงพอใจของผู้ใช้บริการ</h3>
+            <p className="assessment-title">ร่วมประเมินแบบสอบถามความพึงพอใจของผู้ใช้บริการ</p>
             <div className="assessment-button-container">
               <button className="assessment-button">
                 แบบประเมิน
@@ -32,6 +32,30 @@ export default function Home() {
                 <img src="/images/click.png" className="click-icon"></img>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      {/*TRIUP Act*/}
+      <section className="triup-act">
+        <div className="triup-act-section">
+          <div className="triup-act-content">
+            <div className="triup-act-title">
+              <p>ระบบสารสนเทศรองรับพระราชบัญญัติส่งเสริมการใช้ประโยชน์ผลงานวิจัยและนวัตกรรม</p>
+              <p>Information Systems Supporting the Research and Innovation Utilization Promotion Act</p>
+            </div>
+            <div className="triup-act-button-container">
+              <button className="triup-act-button">
+                แบบประเมิน
+              </button>
+              {/* <div className="click-icon-wrapper">
+                <img src="/images/click.png" className="click-icon"></img>
+              </div> */}
+            </div>
+          </div>
+          <div className="triup-act-image">
+            <img src="/images/TRIUP_Act1.png" alt="Assessment Image"></img>
+            <img src="/images/TRIUP_Act2.png" alt="Assessment Image"></img>
           </div>
         </div>
       </section>
